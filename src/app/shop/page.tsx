723db7feca3e810c85d4856781d8d168d9bda13b
Amit Kumar Mandal <itsme.amit1230@gmail.com>
2026-06-02 10:12:22 +0000

    removed filters

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -7,9 +7,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import MobileFilters from "@/components/shop-page/filters/MobileFilters";
-import Filters from "@/components/shop-page/filters";
-import { FiSliders } from "react-icons/fi";
 import { newArrivalsData, relatedProductData, topSellingData } from "../page";
 import ProductCard from "@/components/common/ProductCard";
 import {
@@ -29,18 +26,10 @@ export default function ShopPage() {
         <hr className="h-[1px] border-t-black/10 mb-5 sm:mb-6" />
         <BreadcrumbShop />
         <div className="flex md:space-x-5 items-start">
-          <div className="hidden md:block min-w-[295px] max-w-[295px] border border-black/10 rounded-[20px] px-5 md:px-6 py-5 space-y-5 md:space-y-6">
-            <div className="flex items-center justify-between">
-              <span className="font-bold text-black text-xl">Filters</span>
-              <FiSliders className="text-2xl text-black/40" />
-            </div>
-            <Filters />
-          </div>
           <div className="flex flex-col w-full space-y-5">
             <div className="flex flex-col lg:flex-row lg:justify-between">
               <div className="flex items-center justify-between">
                 <h1 className="font-bold text-2xl md:text-[32px]">Casual</h1>
-                <MobileFilters />
               </div>
               <div className="flex flex-col sm:items-center sm:flex-row">
                 <span className="text-sm md:text-base text-black/60 mr-3">
